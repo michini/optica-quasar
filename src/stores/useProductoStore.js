169f1e04a65912af816a5b42365cfccb61014306
leonel <leonelomar_gc@hotmail.com>
2025-05-29 14:55:28 -0500

    adding products feature

--- src/stores/useProductoStore.js
+++ src/stores/useProductoStore.js
@@ -17,6 +17,28 @@ export const useProductoStore = defineStore('ProductoStore', {
         throw error
       }
     },
+    async getProductSummary() {
+      try {
+        const response = await api.get(`producto/summary/all`, {
+          headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+        })
+        return response.data
+      } catch (error) {
+        console.error(`Error fetching producto:`, error)
+        throw error
+      }
+    },
+    async getProductoByCodigo(codigo) {
+      try {
+        const response = await api.get(`producto/summary/${codigo}`, {
+          headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+        })
+        return response.data
+      } catch (error) {
+        console.error(`Error fetching producto:`, error)
+        throw error
+      }
+    },
   },
 })
 
