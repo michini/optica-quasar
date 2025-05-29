ed4b3172fac7d18da7a33647c1379a8f98009758
Leonel Omar <leonelomar_gc@hotmail.com>
2025-05-29 15:52:16 -0500

    Update AjustesPreCargaPage.vue

--- src/pages/home/ajustes/sections/AjustesPreCargaPage.vue
+++ src/pages/home/ajustes/sections/AjustesPreCargaPage.vue
@@ -354,7 +354,7 @@ import { useAdicionalStore } from 'src/stores/useAdicionalStore'
 import { useProductoStore } from 'src/stores/useProductoStore'
 import { Notify, Dialog } from 'quasar'
 
-const hostname = ref('http://localhost:8000')
+const hostname = ref('http://129.213.136.111:8000')
 const prompt = ref(false)
 const promptAdicional = ref(false)
 const PreCargaStore = usePreCargaStore()
